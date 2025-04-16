85216cb3563106435f4fcb6bacb2bc3c43d3bddf
linweih-alvin <lin.weih@northeastern.edu>
2025-04-15 22:37:23 -0400

    add session storage to keep added items in cart

--- my-app/src/GlobalProvider.js
+++ my-app/src/GlobalProvider.js
@@ -3,8 +3,16 @@ import React, { createContext, useEffect, useState } from "react";
 export const AppContext = createContext();
 
 function GlobalProvider() {
-	const [cartCount, setCartCount] = useState(0);
-	const [addedItemsInCart, setAddedItemsInCart] = useState([]);
+	let cartItemsFromSession = JSON.parse(
+		sessionStorage.getItem("addedItemsInCart")
+	);
+
+	const [cartCount, setCartCount] = useState(
+		cartItemsFromSession ? cartItemsFromSession.length : 0
+	);
+	const [addedItemsInCart, setAddedItemsInCart] = useState(
+		cartItemsFromSession || []
+	);
 	const [cartTotalPrice, setCartTotalPrice] = useState(0);
 
 	useEffect(() => {
