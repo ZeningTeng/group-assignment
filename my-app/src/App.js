855b855ece5f945429ad7776affe8b21c45fae6c
Guhan Santhanam <spguhansanthanam@gmail.com>
2025-04-16 16:24:25 -0400

    Restructred Navbar into sepearte Component & Init Express

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,38 +1,38 @@
 import {
-	BrowserRouter as Router,
-	Routes,
-	Route,
-	Navigate,
-	useLocation,
-	useNavigate,
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+  useLocation,
+  useNavigate,
 } from "react-router-dom";
 import { useEffect } from "react";
-import Home from "./HomePage";
+import Home from "./Pages/HomePage";
 import Result from "./Result";
-import Login from "./login";
+import Login from "./Pages/login";
 import ShoppingCart from "./Components/ShoppingCart";
 
 function App() {
-	const token = localStorage.getItem("token");
-	const location = useLocation();
-	const navigate = useNavigate();
+  const token = localStorage.getItem("token");
+  const location = useLocation();
+  const navigate = useNavigate();
 
-	useEffect(() => {}, [location, navigate]);
+  useEffect(() => {}, [location, navigate]);
 
-	return (
-		<Routes>
-			<Route path="/" element={<Home />} />
-			<Route path="/Result" element={<Result />} />
-			<Route path="/Login" element={<Login />} />
-			<Route path="/cart" element={<ShoppingCart />} />
-		</Routes>
-	);
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/Result" element={<Result />} />
+      <Route path="/Login" element={<Login />} />
+      <Route path="/cart" element={<ShoppingCart />} />
+    </Routes>
+  );
 }
 
 export default function AppWithRouter() {
-	return (
-		<Router>
-			<App />
-		</Router>
-	);
+  return (
+    <Router>
+      <App />
+    </Router>
+  );
 }
