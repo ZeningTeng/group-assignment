e482a93a8f367b32cb89851bcfb35daa1b8cbd80
linweih-alvin <lin.weih@northeastern.edu>
2025-04-16 20:30:32 -0400

    make stacked layout when downsize to smaller screen, add sort function in shopping cart page

---
+++ my-app/src/model/addedItemsInCart_example.js
@@ -0,0 +1,11 @@
+export const productsInCart = [
+	{
+		id: 1,
+		count: 1,
+		productName: "Lovely Cat Face",
+		imagePath: "./assets/img/products/2.png",
+		weight: "30g",
+		material: "gold, white spray paint",
+		price: 30,
+	},
+];
