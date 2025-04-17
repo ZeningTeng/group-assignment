7136d9602585622e9e27b78f93362362d794486f
linweih-alvin <lin.weih@northeastern.edu>
2025-04-16 20:33:40 -0400

    add readyToOrder (a new order after checkout and pay) data structure

---
+++ my-app/src/model/readyToOrder_example.js
@@ -0,0 +1,13 @@
+export const readyToOrder = [
+	{
+		orderId: 1,
+		itemsCount: 1,
+		productLists: [
+			{ productId: 1, count: 2, productName: "Diamond Whale Tail" },
+			{ productId: 2, count: 1, productName: "Lovely Cat Face" },
+		],
+		subtotal: "Lovely Cat Face",
+		shippingFee: "./assets/img/products/2.png",
+		total: "30g",
+	},
+];
