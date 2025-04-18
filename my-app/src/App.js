fce13822c79d9ce26c593b32f0bbf8d04571360e
Guhan Santhanam <spguhansanthanam@gmail.com>
2025-04-18 04:03:36 -0400

    Functional Cart and Email Confirmation for Order is Implemented

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -17,18 +17,21 @@ import SignupPage from "./Pages/SignupPage";
 import AdminPage from "./Pages/AdminPage";
 import SupplierPage from "./Pages/SupplierPage";
 import Unauthorized from "./Pages/Unauthorized";
+import ShopPage from "./Pages/ShopPage";
+import CartPage from "./Pages/CartPage";
 
 function App() { 
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/Result" element={<Result />} />
-      <Route path="/Login" element={<Login />} />
-      <Route path="/cart" element={<ShoppingCart />} />
+      <Route path="/Login" element={<Login />} />     
       <Route path="/signup" element={<SignupPage />} />
       <Route path="/adminDashBoard" element={<AdminPage />} />
       <Route path="/supplierDashBoard" element={<SupplierPage />} />
       <Route path="/unauthorized" element={<Unauthorized />} />
+      <Route path="/shop" element={<ShopPage />} />
+      <Route path="/cart" element={<CartPage />} />
     </Routes>
   );
 }
